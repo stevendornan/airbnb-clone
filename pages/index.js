993466fe9fde592963f0aa377df3dc03f355cd18
Steven Dornan <72313187+stevendornan@users.noreply.github.com>
2022-10-14 18:42:55 +0100

    airbnbbuild

---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import LargeCard from '../components/LargeCard'
+import MediumCard from '../components/MediumCard'
+import SmallCard from '../components/SmallCard'
+
+const Home = ({ exploreData, cardsData }) => {
+  return (
+    <div>
+      <Head>
+        <title>Airbnb</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Banner />
+      <main className='max-w-7xl mx-auto px-8 sm:px-16'>
+        <section className='pt-6'>
+          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+            {exploreData?.map(({img, distance, location}) => (
+              <SmallCard key={img} img={img} distance={distance} location={location} />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
+          <div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+            {cardsData?.map(({img, title}) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
+         <LargeCard img="https://links.papareact.com/4cj" title="The Greatest Outdoors" description="Whishlists created by Airbnb." buttonText="Get Inspired" />
+        </section>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export async function getStaticProps(){
+  const https = require('https');
+  const agent = new https.Agent({
+    rejectUnauthorized: false
+  });
+  const res1 = await fetch('https://links.papareact.com/pyp', {
+    method: 'GET',
+    agent
+  })
+  const exploreData = await res1.json()
+
+  const res2 = await fetch('https://links.papareact.com/zp1', {
+    method: 'GET',
+    agent
+  })
+  const cardsData = await res2.json()
+
+
+  return {
+    props:{
+      exploreData,
+      cardsData
+    }
+  }
+}
+
+export default Home
